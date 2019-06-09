e2018ff8361a2869250f70fd41e0e32e7956e12e
user2 <jaimelunaec@hotmail.com>
2019-06-09 15:27:05 -0500

    npm update

---
+++ app/store/configureStore.js
@@ -0,0 +1,35 @@
+import { createStore, compose, applyMiddleware } from 'redux'
+import { persistStore, persistCombineReducers } from 'redux-persist'
+import storage from 'redux-persist/es/storage' // default: localStorage if web, AsyncStorage if react-native
+import createSagaMiddleware from 'redux-saga'
+
+import rootReducers from 'app/reducers' // where reducers is a object of reducers
+import sagas from 'app/sagas'
+
+const config = {
+  key: 'root',
+  storage,
+  blacklist: ['nav', 'loadingReducer'],
+  debug: true, //to get useful logging
+}
+
+const middleware = []
+const sagaMiddleware = createSagaMiddleware()
+
+middleware.push(sagaMiddleware)
+
+const reducers = persistCombineReducers(config, rootReducers)
+const enhancers = [applyMiddleware(...middleware)]
+// const initialState = {};
+const persistConfig = { enhancers }
+const store = createStore(reducers, undefined, compose(...enhancers))
+const persistor = persistStore(store, persistConfig, () => {
+  //   console.log('Test', store.getState());
+})
+const configureStore = () => {
+  return { persistor, store }
+}
+
+sagaMiddleware.run(sagas)
+
+export default configureStore
